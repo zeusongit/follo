fbcaf302666f84b707d8d1e7e4a947f82563f0d9
Ashish <aggarwal.ash@husky.neu.edu>
2019-04-13 17:25:54 -0400

    added signup,header, nav, sidebar

--- folloui/src/app/app.module.ts
+++ folloui/src/app/app.module.ts
@@ -2,11 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
+import { SignupComponent } from './components/signup/signup.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SignupComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
