8f72edc532466d5d27eab10cba83836b0897510c
Ashish <aggarwal.ash@husky.neu.edu>
2019-04-14 17:50:00 -0400

    color change

--- folloui/src/app/app.module.ts
+++ folloui/src/app/app.module.ts
@@ -7,13 +7,15 @@ import { AppComponent } from './app.component';
 import { SignupComponent } from './components/signup/signup.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { LeftSidebarComponent } from './components/left-sidebar/left-sidebar.component';
+import { PostsComponent } from './components/posts/posts.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     SignupComponent,
     NavbarComponent,
-    LeftSidebarComponent
+    LeftSidebarComponent,
+    PostsComponent
   ],
   imports: [
     BrowserModule,
