61f7ad9bd319610bd0eb97345c4c7644ad568723
singh-harsh <singh.har@husky.neu.edu>
2019-04-21 18:12:16 -0400

    updated view comm details

--- folloui/src/app/app-routing.module.ts
+++ folloui/src/app/app-routing.module.ts
@@ -1,9 +1,5 @@
-import { MainComponent } from './main/main.component';
 import { ViewCommunityDetailComponent } from './components/view-community-detail/view-community-detail.component';
 import { CreateCommunityComponent } from './components/create-community/create-community.component';
-import { HomeComponent } from './components/home/home.component';
-import { MainContentGridComponent } from './components/main-content-grid/main-content-grid.component';
-import { AppComponent } from './app.component';
 import { SignupComponent } from './components/signup/signup.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
@@ -11,12 +7,12 @@ import { SigninComponent } from './components/signin/signin.component';
 import { ContentComponent } from './components/content/content.component'
 
 const routes: Routes = [
-  { path: '', redirectTo: 'content', pathMatch: 'full'},
+  { path: '', redirectTo: 'content', pathMatch: 'full' },
   { path: 'login', component: SigninComponent },
   { path: 'signup', component: SignupComponent },
   { path: 'community/create', component: CreateCommunityComponent },
-  { path: 'community/:commname',  component: ViewCommunityDetailComponent },
-  { path: 'content', component: ContentComponent}
+  { path: 'community/:commname', component: ViewCommunityDetailComponent },
+  { path: 'content', component: ContentComponent }
 ];
 
 @NgModule({
