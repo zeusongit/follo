b8c6688e96610f35bc8009442e17f7c58f33cb97
Vikas Singh <singh.vika@northeastern.edu>
2019-04-23 19:47:24 -0400

    fixing post object construction

--- folloui/src/app/services/post.service.ts
+++ folloui/src/app/services/post.service.ts
@@ -10,13 +10,33 @@ export class PostService {
   constructor(private http: HttpClient) { }
 
   createPost(post: Post, commName: string, token: string) {
-    return this.http.post<any>('http://localhost:3000/community/' + commName + '/', post, {
-      headers:
-      {
-        'Content- Type': 'application/ json',
-        // tslint:disable-next-line:object-literal-key-quotes
-        'Authorization': 'Bearer ' + token
-      },
+    console.log('post from component');
+    console.log(post);
+    let headers;
+    const formData = new FormData();
+    formData.append('type', post.type);
+    formData.append('title', post.title);
+    formData.append('content', post.content);
+    if (post.type === 'image') {
+      // handle adding of image in formData here
+    }
+
+    switch(post.type){
+      case 'text': 
+        headers = {
+          'Authorization': 'Bearer ' + token
+        }
+        break;
+      
+      default: 
+        headers = {
+          'Authorization': 'Bearer ' + token
+        }  
+    }
+    console.log('sending post');
+    console.log(post)
+    return this.http.post<any>('http://localhost:3000/community/' + commName + '/post/', formData, {
+      headers: headers,
       observe: 'response'
     });
   }
