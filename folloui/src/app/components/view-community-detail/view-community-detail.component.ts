a188bc9e2eae02bbbffc2151e548d2b5273bef55
singh-harsh <singh.har@husky.neu.edu>
2019-04-23 01:14:34 -0400

    image and button flicker fixed

--- folloui/src/app/components/view-community-detail/view-community-detail.component.ts
+++ folloui/src/app/components/view-community-detail/view-community-detail.component.ts
@@ -1,3 +1,5 @@
+import { Observable } from 'rxjs';
+import { UserService } from './../../services/user.service';
 import { Store } from '@ngrx/store';
 import { CommunityService } from './../../services/community.service';
 import { Post } from './../../models/post';
@@ -15,11 +17,17 @@ export class ViewCommunityDetailComponent implements OnInit {
   communityName: string;
   authToken: string;
   community: any;
-  constructor(private route: ActivatedRoute, private commService: CommunityService, private store: Store<any>) { }
+  currentUserName: Observable<string>;
+  followingCommunities: any;
+  authUser: any;
+  canFollow: boolean;
+  commPicture: string;
+  // tslint:disable-next-line:max-line-length
+  constructor(private route: ActivatedRoute, private userService: UserService, private commService: CommunityService, private store: Store<any>) { }
 
   ngOnInit() {
     console.log('INSIDE VIEW DETAILS');
-
+    this.canFollow = false;
     this.route.params.subscribe(name => {
       this.community = [];
       this.posts = [];
@@ -28,11 +36,27 @@ export class ViewCommunityDetailComponent implements OnInit {
     });
   }
 
+  getUserDetails() {
+    if (this.authToken != null) {
+
+      this.userService.getUserDetail(this.authUser.token, this.authUser.email).toPromise()
+        .then(res => {
+          if (res.status === 200) {
+            this.followingCommunities = res.body.followingCommunities;
+          }
+        }).catch(err => {
+          console.log('ERROR GETTING USER DATA', err);
+        });
+    }
+  }
+
   viewCommunityDetails() {
     this.store.select('userAuth').subscribe((userAuth) => {
       console.log(`TOKENS STATUS CHANGED IN VIEW COMMUNITY: ${userAuth}`);
       console.log(userAuth);
+      this.authUser = userAuth;
       this.authToken = userAuth.token;
+      this.currentUserName = userAuth.username;
     });
 
     this.commService.getCommunityDetails(this.communityName, this.authToken).toPromise()
@@ -40,6 +64,9 @@ export class ViewCommunityDetailComponent implements OnInit {
         if (res.status === 200) {
           console.log('RESPONSE FOR GET COMM DETAILS ', res);
           this.community = res.body;
+          this.canFollow = this.community.createdBy.user.username === this.currentUserName ? false : true;
+          this.commPicture = (this.community.communityPicture != null) ?
+            this.community.communityPicture : '../../../assets/images/create-community-header.png';
         }
       }).catch(err => {
         console.log('ERROR GETTING COMMUNITY DETAILS', err);
