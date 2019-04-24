96dca7aa75e756eeebe0731fd5621ee96f7fffd6
Vikas Singh <singh.vika@northeastern.edu>
2019-04-24 03:30:57 -0400

    adding discovered view

--- folloui/src/app/components/content/content.component.ts
+++ folloui/src/app/components/content/content.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Post } from 'src/app/models/post';
+import { UserService } from '../../services/user.service';
+
 
 @Component({
   selector: 'app-content',
@@ -8,12 +10,38 @@ import { Post } from 'src/app/models/post';
 })
 export class ContentComponent implements OnInit {
 
+  currentDiscoverPage: number = 0;
+  currentPersonalPage: number = 0;
+  discoverPosts: Array<Post> = [];
+  personalPosts: Array<Post> = [];
+  viewToDisplay: string = 'discover'
 
-  discoverPosts: Array<Post>;
-  personalPosts: Array<Post>
-  constructor() { }
+  constructor(private userService: UserService) { }
 
-  ngOnInit() {
+  ngOnInit() { 
+    this.fetchDiscoverPosts();
+  }
+
+
+  fetchDiscoverPosts() {
+     this.userService.getDiscoverPosts(this.currentDiscoverPage)
+     .then(res => {
+       console.log('discovered posts');
+       this.currentDiscoverPage += this.currentDiscoverPage + 1;
+       console.log(res.body.posts);
+       this.discoverPosts = [...this.discoverPosts,...res.body.posts];
+     })
+     .catch(err => {
+       console.log(err);
+     })
+  }
+
+  fetchPersonalPosts() {
+
+  }
+
+  setView(view: string) { 
+    this.viewToDisplay = view;
   }
 
 }
